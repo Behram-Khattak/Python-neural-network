b13b2f896c218d4f0c2fd12064e88c62c58d23e3
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-09-30 12:49:53 +0500

    Working on creating neural network

---
+++ neural-network.py
@@ -0,0 +1,12 @@
+print_number = "Enter your first number";
+
+print_symbol = "Enter your symbol on which you want to calculate";
+
+print_sec_number = "Enter your second number";
+
+print(print_number);
+
+get_number = input();
+
+print(print_symbol);
+
